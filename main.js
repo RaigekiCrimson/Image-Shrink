159382bd929dbff6e500c7b8b1c6c22a971613b6
Will Parson II <RaigekiCrimson@outlook.com>
2020-06-03 15:47:50 -0400

    added controls for image quality

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const path = require('path');
+const os = require('os');
+const {app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
 
 // Set Environment
 process.env.NODE_ENV = "development";
@@ -10,113 +16,135 @@ let mainWindow;
 let aboutWidow;
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    title: "ImageShrink",
-    width: isDev ? 800 : 500,
-    height: 600,
-    // icon: "./assets/icon/Icon_256x256.png",
-    resizable: isDev ? true : false,
-    backgroundColor: "white",
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+    mainWindow = new BrowserWindow({
+        title: "ImageShrink",
+        width: isDev ? 800 : 500,
+        height: 600,
+        // icon: "./assets/icon/Icon_256x256.png",
+        resizable: isDev ? true : false,
+        backgroundColor: "white",
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
 
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
-  }
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
 
-  mainWindow.loadFile("./app/index.html");
+    mainWindow.loadFile("./app/index.html");
 }
 
 function createAboutWindow() {
-  aboutWidow = new BrowserWindow({
-    title: " About ImageShrink",
-    width: 300,
-    height: 300,
-    // icon: "./assets/icon/Icon_256x256.png",
-    resizable: false,
-    backgroundColor: "white",
-  });
+    aboutWidow = new BrowserWindow({
+        title: " About ImageShrink",
+        width: 300,
+        height: 300,
+        // icon: "./assets/icon/Icon_256x256.png",
+        resizable: false,
+        backgroundColor: "white",
+    });
 
-  aboutWidow.loadFile("./app/about.html");
+    aboutWidow.loadFile("./app/about.html");
 }
 
 app.on("ready", () => {
-  createMainWindow();
-  const mainMenu = Menu.buildFromTemplate(menu);
-  Menu.setApplicationMenu(mainMenu);
+    createMainWindow();
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
 
-  mainWindow.on("ready", () => (mainWindow = null));
+    mainWindow.on("ready", () => (mainWindow = null));
 });
 
 // Menu Template
 const menu = [
-  ...(isMac
-    ? [
-        {
-          label: app.name,
-          submenu: [
+    ...(isMac
+        ? [
             {
-              label: "About",
-              click: createAboutWindow,
+                label: app.name,
+                submenu: [
+                    {
+                        label: "About",
+                        click: createAboutWindow,
+                    },
+                ],
             },
-          ],
-        },
-      ]
-    : []),
+        ]
+        : []),
 
-  {
-    role: "fileMenu",
-  },
-  ...(!isMac
-    ? [
-        {
-          label: "Help",
-          submenu: [
+    {
+        role: "fileMenu",
+    },
+    ...(!isMac
+        ? [
             {
-              label: "About",
-              click: createAboutWindow,
+                label: "Help",
+                submenu: [
+                    {
+                        label: "About",
+                        click: createAboutWindow,
+                    },
+                ],
             },
-          ],
-        },
-      ]
-    : []),
-  ...(isDev
-    ? [
-        {
-          label: "Developer",
-          submenu: [
+        ]
+        : []),
+    ...(isDev
+        ? [
             {
-              role: "reload",
+                label: "Developer",
+                submenu: [
+                    {
+                        role: "reload",
+                    },
+                    {
+                        role: "forcereload",
+                    },
+                    {
+                        type: "separator",
+                    },
+                    {
+                        role: "toggledevtools",
+                    },
+                ],
             },
-            {
-              role: "forcereload",
-            },
-            {
-              type: "separator",
-            },
-            {
-              role: "toggledevtools",
-            },
-          ],
-        },
-      ]
-    : []),
+        ]
+        : []),
 ];
 
 ipcMain.on("image:minimize", (e, options) => {
-  console.log(options);
+    options.dest = path.join(os.homedir(), 'imageshrink');
+    shrinkImage(options);
 });
 
+async function shrinkImage({ imgPath: imgPath, quality, dest }) {
+    try {
+        const pngQuality = quality / 100
+        const files = await imagemin([slash(imgPath)], {
+            destination: dest,
+            plugins: [
+                imageminMozjpeg({ quality }),
+                imageminPngquant({
+                    quality: [pngQuality, pngQuality]
+                })
+            ]
+        })
+
+        console.log(files)
+        shell.openPath(dest)
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 app.on("window-all-closed", () => {
-  if (!isMac) {
-    app.quit();
-  }
+    if (!isMac) {
+        app.quit();
+    }
 });
 
 app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow();
-  }
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow();
+    }
 });
